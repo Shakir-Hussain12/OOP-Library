857f53c8adf8899a80f9d801984ebc474fc5b5d8
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-04-15 10:18:44 +0500

    Added check to students Class

--- student.rb
+++ student.rb
@@ -6,8 +6,12 @@ class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
-    classroom.students << self
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.includes?(self);
+  end 
 
   def play_hooky
     '¯(ツ)/¯'
